1eb585e85dd667d1d5874dee2cc7f32047736ef5
Samuel Noye <samuel.noye@amalitech.org>
2022-05-23 10:44:00 +0000

    setup

--- LineChart-UIKit/ViewController.swift
+++ LineChart-UIKit/ViewController.swift
@@ -1,19 +1,33 @@
 //
 //  ViewController.swift
-//  LineChart-UIKit
+//  BarChart-UIKit
 //
 //  Created by Samuel Noye on 23/05/2022.
 //
 
 import UIKit
+import Charts
+import TinyConstraints
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate {
 
+    
+    lazy var lineChartView: LineChartView = {
+        let chartView = LineChartView()
+        chartView.backgroundColor = .systemBlue
+        return chartView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(lineChartView)
+        lineChartView.centerInSuperview()
+        lineChartView.width(to: view)
+        lineChartView.heightToWidth(of: view)
     }
 
-
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        <#code#>
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LineChart-UIKit' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LineChart-UIKit
+   pod 'Charts'
+   pod 'TinyConstraints'
+end
