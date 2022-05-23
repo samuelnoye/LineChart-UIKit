8fd93fad7828ea20a0121bb84c95ab14c853af67
Samuel Noye <samuel.noye@amalitech.org>
2022-05-23 11:12:53 +0000

    Update ViewController.swift

--- LineChart-UIKit/ViewController.swift
+++ LineChart-UIKit/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, ChartViewDelegate {
     lazy var lineChartView: LineChartView = {
         let chartView = LineChartView()
         chartView.backgroundColor = .systemBlue
+        
+        chartView.rightAxis.enabled = false
         return chartView
     }()
     
@@ -25,9 +27,36 @@ class ViewController: UIViewController, ChartViewDelegate {
         lineChartView.centerInSuperview()
         lineChartView.width(to: view)
         lineChartView.heightToWidth(of: view)
+        setDaata()
     }
 
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
-        <#code#>
+        print(entry)
     }
+    
+    func setDaata(){
+        let set1 = LineChartDataSet(entries: yValues, label: "Speed")
+        
+        let data = LineChartData(dataSet: set1)
+        lineChartView.data = data
+    }
+    
+    let yValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0.0, y: 10.0),
+        ChartDataEntry(x: 1.0, y: 5.0),
+        ChartDataEntry(x: 2.0, y: 7.0),
+        ChartDataEntry(x: 3.0, y: 5.0),
+        ChartDataEntry(x: 4.0, y: 10.0),
+        ChartDataEntry(x: 5.0, y: 6.0),
+        ChartDataEntry(x: 6.0, y: 5.0),
+        ChartDataEntry(x: 7.0, y: 7.0),
+        ChartDataEntry(x: 8.0, y: 8.0),
+        ChartDataEntry(x: 9.0, y: 12.0),
+        ChartDataEntry(x: 10.0, y: 10.0),
+        ChartDataEntry(x: 11.0, y: 6.0),
+        ChartDataEntry(x: 12.0, y: 5.0),
+        ChartDataEntry(x: 13.0, y: 3.0),
+        ChartDataEntry(x: 14.0, y: 15.0),
+        ChartDataEntry(x: 15.0, y: 6.0)
+    ]
 }
